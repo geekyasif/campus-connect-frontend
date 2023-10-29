5ee2c33a848dd4a1a0da677cc06f68e9c55ebb47
geekyasif <mohdasif.dev@gmail.com>
2023-10-30 05:17:27 +0530

    working on new ui of user profile

--- src/components/UserProfile/UserMyProfile/UserMyProfileSidebar.js
+++ src/components/UserProfile/UserMyProfile/UserMyProfileSidebar.js
@@ -1,30 +1,144 @@
-
 import React from "react";
 import Avatar from "react-avatar";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function UserMyProfileSidebar() {
   const { user } = useSelector((state) => state.auth);
   return (
-    <div className="border rounded p-2 text-center bg-white shadow md:w-[300px] md:h-[400px] mx-auto w-full">
-      <div className="my-4">
-        {user?.user?.personal_details?.profile_url !== "" ? (
-          <img
-alt="profile"
-            src={user?.user?.personal_details?.profile_url}
-            className=" w-[120px] lg:w-[150px] rounded-full mx-auto"
-          />
-        ) : (
-          <Avatar
-            name={user?.user?.personal_details?.fullName}
-            size={95}
-            round={true}
-          />
-        )}
+    <div className="border rounded">
+      {/* user profile cover change section  */}
+      <div className="w-full h-[180px] rounded-t-md relative bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+        <button className="absolute top-3 right-3 px-2 py-1 bg-white text-blue-500 rounded text-xs">
+          change cover
+        </button>
       </div>
 
-      <p className="text-lg font-bold">{user?.user?.personal_details?.fullName}</p>
-      <p className="text-xs lg:text-sm text-gray-500 font-semibold">@{user?.user?.personal_details?.username}</p>
+      {/* about section  */}
+      <div className="">
+        {/* profile image section  */}
+        <div className="h-[100px] flex justify-center relative">
+          <div className="my-4 bg-gray-100 rounded-full p-[3px] w-[150px] h-[150px] lg:w-[150px] absolute bottom-0">
+            {user?.user?.personal_details?.profile_url !== "" ? (
+              <img
+                alt="profile"
+                src={user?.user?.personal_details?.profile_url}
+                className="rounded-full  w-[150px] h-[150px] lg:w-[150px] object-cover"
+              />
+            ) : (
+              <Avatar
+                name={user?.user?.personal_details?.fullName}
+                size={95}
+                round={true}
+              />
+            )}
+          </div>
+        </div>
+
+        {/* user profile about section  */}
+        <div className="p-2">
+          <div className="flex flex-row justify-between">
+            <div>
+              <p className="text-lg font-bold">
+                {user?.user?.personal_details?.fullName}
+              </p>
+              <p className="text-xs lg:text-sm text-gray-500 font-semibold">
+                {user?.user?.personal_details?.expertise_in}
+              </p>
+            </div>
+
+            <div className="flex gap-2">
+              <button className="px-2 rounded bg-gray-100 text-xs">
+                Share
+              </button>
+              <button className="px-2 rounded bg-gray-100 text-xs">
+                Edit Profile
+              </button>
+            </div>
+          </div>
+
+          <div className="py-2">
+            <p className="text-xs lg:text-sm text-gray-500 font-semibold">
+              {user?.user?.personal_details?.city}
+            </p>
+            <div className="flex flex-row gap-2 border-b-2 my-4 pb-2">
+              {user?.user?.personal_details?.skills.split(",").map((skill) => (
+                <p className="px-2 py-1 text-xs rounded bg-gray-100">{skill}</p>
+              ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="flex flex-wrap gap-1">
+                <a
+                  className="rounded p-1 text-xs  lg:text-sm  border"
+                  href={user?.user?.social_links?.github}
+                >
+                  github
+                </a>
+                <a
+                  className="rounded p-1 text-xs  lg:text-sm  border"
+                  href={user?.user?.social_links?.linkedin}
+                >
+                  linkedin
+                </a>
+                <a
+                  className="rounded p-1 text-xs  lg:text-sm  border"
+                  href={user?.user?.social_links?.leetcode}
+                >
+                  leetcode
+                </a>
+                <a
+                  className="rounded p-1 text-xs  lg:text-sm  border"
+                  href={user?.user?.social_links?.portfolio}
+                >
+                  portfolio
+                </a>
+                <a
+                  className="rounded p-1 text-xs  lg:text-sm  border"
+                  href={user?.user?.social_links?.hackerrank}
+                >
+                  hackerrank
+                </a>
+                <a
+                  className="rounded p-1 text-xs  lg:text-sm  border"
+                  href={user?.user?.social_links?.codechef}
+                >
+                  codechef
+                </a>
+                <a
+                  className="rounded p-1 text-xs  lg:text-sm  border"
+                  href={user?.user?.social_links?.geeksforgeek}
+                >
+                  geeksforgeek
+                </a>
+              </div>
+
+              <div>
+                <p className="px-2 bg-gray-100 rounded text-xs py-1">Contact</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* tabs / navigation section  */}
+      {/* <div className="flex gap-2 overflow-x-auto border-t-[2px] pt-2">
+        <Link className="px-2 mb-3 text-sm font-semibold" to={`#about`}>
+          About
+        </Link>
+        <Link className="px-2 mb-3 text-sm font-semibold" to="#queries">
+          Queries
+        </Link>
+        <Link className="px-2 mb-3 text-sm font-semibold" to="#academics">
+          Academics
+        </Link>
+        <Link className="px-2 mb-3 text-sm font-semibold" to="#certificates">
+          Certificates
+        </Link>
+        <Link className="px-2 mb-3 text-sm font-semibold" to="#projects">
+          Projects
+        </Link>
+      </div> */}
     </div>
   );
 }
--- src/pages/User/MyProfile/UserMyProfile.js
+++ src/pages/User/MyProfile/UserMyProfile.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Avatar from "react-avatar";
 import { useDispatch, useSelector } from "react-redux";
 import UserProfileTab from "../../../components/UserProfile/UserMyProfile/UserProfileTab";
 import UserTabContainer from "../../../components/UserProfile/UserMyProfile/UserTabContainer";
@@ -7,8 +6,9 @@ import UserMyProfileSidebar from "../../../components/UserProfile/UserMyProfile/
 import { handleIsSideNavbarOpen } from "../../../features/authSlice";
 
 function UserMyProfile() {
-  const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const [isReadMore, setIsReadMore] = useState(false);
 
   const [activeTab, setActiveTavb] = useState("About");
   const [activeContainerData, setActiveContainerData] = useState("About");
@@ -18,19 +18,149 @@ function UserMyProfile() {
   };
 
   useEffect(() => {
-    dispatch(handleIsSideNavbarOpen());
+    // dispatch(handleIsSideNavbarOpen());
   }, []);
 
   return (
-    <div className="container mx-auto my-2">
-      <div className="flex lg:flex-row flex-col p-2 gap-2">
-        <UserMyProfileSidebar />
-        <div className="flex flex-col flex-wrap border p-2 rounded w-full bg-white shadow ">
+    <div className="container mx-auto p-2">
+      <UserMyProfileSidebar />
+      {/* <div className="flex flex-col flex-wrap border p-2 rounded w-full bg-white shadow ">
           <UserProfileTab
             handleActiveTab={handleActiveTab}
             activeTab={activeTab}
           />
           <UserTabContainer activeTab={activeTab} />
+        </div> */}
+
+      {/* user about container  */}
+      <div className="my-2 rounded bg-white border" id={`#about`}>
+        <div className="p-4">
+          <p>About</p>
+        </div>
+        <div className="px-4 py-2">
+          <p
+            className={
+              isReadMore
+                ? "line-clamp-none h-full"
+                : "line-clamp-3 h-[100px] overflow-hidden"
+            }
+          >
+            {user?.user?.personal_details?.about}{" "}
+            <span>{isReadMore ? "" : "..."}</span>
+          </p>
+          <button
+            onClick={() => setIsReadMore(!isReadMore)}
+            className="text-blue-500 text-sm"
+          >
+            {isReadMore ? "Read less" : "Read more"}
+          </button>
+        </div>
+      </div>
+
+      {/* user queries container  */}
+      <div className="my-2 rounded bg-white border" id={`#queries`}>
+        <div className="p-4 flex justify-between items-center">
+          <p className="font-semibold">Queries</p>
+          <button className="text-xs">Add Queries</button>
+        </div>
+        <div className="px-4 py-2">
+          <p
+            className={
+              isReadMore
+                ? "line-clamp-none h-full"
+                : "line-clamp-3 h-[100px] overflow-hidden"
+            }
+          >
+            {user?.user?.personal_details?.about}{" "}
+            <span>{isReadMore ? "" : "..."}</span>
+          </p>
+          <button
+            onClick={() => setIsReadMore(!isReadMore)}
+            className="text-blue-500 text-sm"
+          >
+            {isReadMore ? "Read less" : "Read more"}
+          </button>
+        </div>
+      </div>
+
+      {/* user academics container  */}
+      <div className="my-2 rounded bg-white border" id={`#academics`}>
+        <div className="p-4 flex justify-between items-center">
+          <p className="font-semibold">Academics</p>
+          <button className="text-xs">Add Academic</button>
+        </div>
+
+        <div className="px-4 py-2">
+          <p
+            className={
+              isReadMore
+                ? "line-clamp-none h-full"
+                : "line-clamp-3 h-[100px] overflow-hidden"
+            }
+          >
+            {user?.user?.personal_details?.about}{" "}
+            <span>{isReadMore ? "" : "..."}</span>
+          </p>
+          <button
+            onClick={() => setIsReadMore(!isReadMore)}
+            className="text-blue-500 text-sm"
+          >
+            {isReadMore ? "Read less" : "Read more"}
+          </button>
+        </div>
+      </div>
+
+      {/* user certificates container  */}
+      <div className="my-2 rounded bg-white border" id={`#certificates`}>
+        <div className="p-4 flex justify-between items-center">
+          <p className="font-semibold">Certificates</p>
+          <button className="text-xs">Add Certificate</button>
+        </div>
+
+        <div className="px-4 py-2">
+          <p
+            className={
+              isReadMore
+                ? "line-clamp-none h-full"
+                : "line-clamp-3 h-[100px] overflow-hidden"
+            }
+          >
+            {user?.user?.personal_details?.about}{" "}
+            <span>{isReadMore ? "" : "..."}</span>
+          </p>
+          <button
+            onClick={() => setIsReadMore(!isReadMore)}
+            className="text-blue-500 text-sm"
+          >
+            {isReadMore ? "Read less" : "Read more"}
+          </button>
+        </div>
+      </div>
+
+      {/* user projects container  */}
+      <div className="my-2 rounded bg-white border" id={`#projects`}>
+        <div className="p-4 flex justify-between items-center">
+          <p className="font-semibold">Projects</p>
+          <button className="text-xs">Add Project</button>
+        </div>
+
+        <div className="px-4 py-2">
+          <p
+            className={
+              isReadMore
+                ? "line-clamp-none h-full"
+                : "line-clamp-3 h-[100px] overflow-hidden"
+            }
+          >
+            {user?.user?.personal_details?.about}{" "}
+            <span>{isReadMore ? "" : "..."}</span>
+          </p>
+          <button
+            onClick={() => setIsReadMore(!isReadMore)}
+            className="text-blue-500 text-sm"
+          >
+            {isReadMore ? "Read less" : "Read more"}
+          </button>
         </div>
       </div>
     </div>
